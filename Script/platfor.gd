80fe6dbe20f68496cec002482868e2e94d972c9a
BohdanPoz <bohdanpoznanskyi08@gmail.com>
2025-01-18 01:37:37 +0000

    Update

--- Script/platfor.gd
+++ Script/platfor.gd
@@ -3,9 +3,14 @@ extends RigidBody3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	linear_velocity.x -= 0.1
+	position.x -= 0.5
+	
+	if position.x <= -150:
+		print(position.x, name)
+		position.x = 150
+	
